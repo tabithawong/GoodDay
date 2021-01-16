af12a9608161ebeee454080f699a42df2b15259f
Tabitha Wong <tabi159w@gmail.com>
2021-01-16 14:08:39 -0500

    change quote button

--- browserAction/src/scripts/popup.js
+++ browserAction/src/scripts/popup.js
@@ -14,21 +14,8 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('clickMe').addEventListener("click", isFrogHidden)
     document.getElementById('fireContentScript').addEventListener("click", fireContentScript)
     document.getElementById('button').addEventListener("click", addImages)
-    document.getElementBtId('qbutton').addEventListener("click", changeQuote)
 })
 
-function getRandomInt(){
-    return Math.floor(Math.random() * Math.floor(max)); 
-}
-
-function changeQuote() {
-    if (!document.getElementById) return;
-    var quotes = ["Enjoy the little things!", "You can & you will", "Drink some water!", "Go grab a snack!", "Send a text to a friend!",
-        "Rest your eyes for 10 seconds!", "Go for a walk and enjoy some sunlight!", "Today's a good day! :)", "Listen to your favourite song!", "Take a break from the screen!"];
-    var newquote = quotes[getRandomInt(9)];
-    var quote = document.getElementById("quote");
-    quote.firstChild.nodeValue=newquote;
-}
 
 function addImages() {
     var url = "https://source.unsplash.com/150x150?";
@@ -61,6 +48,7 @@ function newItem() {
 
 document.body.onkeyup = function(e) {
   if (e.keyCode == 13) {
+    console.log("enter clicked!");
     newItem();
   }
 };
