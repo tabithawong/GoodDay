a1672dbf0b0ddd05028df879e40f8a85e43f269e
Julianne Jorda <julianne.jorda@yahoo.com>
2021-01-16 14:50:19 -0500

    fixed quote

--- browserAction/src/scripts/popup.js
+++ browserAction/src/scripts/popup.js
@@ -14,8 +14,23 @@ function fireContentScript() {
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById("checkboxImg").addEventListener('click', checkCheckbox)
     document.getElementById('button').addEventListener("click", addImages)
-    document.getElementById('qbutton').addEventListener("click", changeQuote)
+    // document.getElementById('qbutton').addEventListener("click", changeQuote)
 })
+
+// function getRandomInt() {
+//     return Math.floor(Math.random() * Math.floor(max)); 
+//   }
+  var quotes = ["Enjoy the little things!", "You can & you will", "Drink some water!", "Go grab a snack!", "Send a text to a friend!",
+      "Rest your eyes for 10 seconds!", "Go for a walk and enjoy some sunlight!", "Today's a good day! :)", "Listen to your favourite song!", "Take a break from the screen!"];
+//   var newquote = quotes[getRandomInt(9)];
+//   var quote = document.getElementById("quote");
+//   quote.textContent = newquote; 
+//   console.log(newquote)
+
+// random quote index
+const quotes_i = Math.floor(Math.random() * quotes.length);
+// set a quote
+document.getElementById("quote").innerHTML = quotes[quotes_i];
 
 
 function addImages() {
