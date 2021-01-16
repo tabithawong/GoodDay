904ac03e21dfa0de61716840bc2fba97be83de26
Tabitha Wong <tabi159w@gmail.com>
2021-01-16 14:43:32 -0500

    Update popup.js

--- browserAction/src/scripts/popup.js
+++ browserAction/src/scripts/popup.js
@@ -12,18 +12,6 @@ function fireContentScript() {
 
 
 document.addEventListener('DOMContentLoaded', function() {
-    
-    function getRandomInt() {
-        return Math.floor(Math.random() * Math.floor(max)); 
-      }
-      
-    function changeQuote() {
-        if (!document.getElementById) return;
-        var quotes = ["Enjoy the little things!", "You can & you will", "Drink some water!", "Go grab a snack!", "Send a text to a friend!",
-            "Rest your eyes for 10 seconds!", "Go for a walk and enjoy some sunlight!", "Today's a good day! :)", "Listen to your favourite song!", "Take a break from the screen!"];
-        var newquote = quotes[getRandomInt(9)];
-        var quote = document.getElementById("quote");
-        quote.value=newquote; 
     document.getElementById("checkboxImg").addEventListener('click', checkCheckbox)
     document.getElementById('button').addEventListener("click", addImages)
     document.getElementById('qbutton').addEventListener("click", changeQuote)
