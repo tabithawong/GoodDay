5a0fea0c0b639438168e56aea7396e493dab2e78
Tabitha Wong <tabi159w@gmail.com>
2021-01-16 23:33:33 -0500

    deleted frog function

--- extension/src/scripts/popup.js
+++ extension/src/scripts/popup.js
@@ -1,9 +1,3 @@
-function isFrogHidden() {
-    const frog = document.getElementById('frog')
-    const style = window.getComputedStyle(frog)
-    frog.style.display = (style.display === 'none') ? 'block' : 'none'
-}
-
 function fireContentScript() {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, { fireContentScript: true, type: 'fireContentScript' })
